a3834daef5d7a88e246b94f40c3c21701f76828f
onawli <prhrk753vptn9571@126.com>
2021-06-06 18:42:44 +0800

    做了一些样式渲染了了一些数据

--- fore-end/src/views/Films.vue
+++ fore-end/src/views/Films.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="films-list">
+    <!-- /轮播图 -->
     <div class="swiper-container">
         <div class="swiper-wrapper">
             <div class="swiper-slide">
@@ -18,11 +19,16 @@
         <!-- 如果需要分页器 -->
         <div class="swiper-pagination"></div>
     </div>
+    <!-- /轮播图 -->
+
+    <!-- 定位城市 -->
     <div class="city-fixed">
       <span>{{ curCity }}</span>
       <i class="iconfont icon-jiantou-copy-copy
       "></i>
     </div>
+    <!-- 定位城市 -->
+
     <div class="tab-bar-wrapper">
       <ul class="tab-bar">
         <li class="z-act">
@@ -32,32 +38,98 @@
           <span>即将上映</span>
         </li>
         <div class="tabs-slide">
-                        <span></span>
-                    </div>
+            <span></span>
+        </div>
       </ul>
     </div>
-    <div class="movies"></div>
+
+    <!-- list -->
+    <div class="film-list-content">
+      <ul>
+        <li
+        v-for="(item, index) in films"
+        :key="index">
+          <div class="img">
+            <img :src="item.poster" alt="">
+          </div>
+          <div class="info">
+            <div>
+              <span class="name">
+                {{ item.name }}
+              </span>
+              <span class="type">
+                {{ item.filmType.name }}
+              </span>
+            </div>
+            <div>
+              <span class="label">观众评分</span>
+              <span class="grade">{{ item.grade }}</span>
+            </div>
+            <div>
+              <span class="label">主演：{{ actorsList(item.actors) }}</span>
+            </div>
+            <div>
+              <span class="label">{{ item.nation }} | {{ item.runtime }}分钟</span>
+            </div>
+            </div>
+              <div class="buy">购票</div>
+        </li>
+      </ul>
+    </div>
+    <!-- list -->
   </div>
 </template>
 
 <script>
 import Swiper from 'swiper';
+import axios from 'axios';
+
 export default {
   name: 'Films',
 
   data () {
     return {
-      curCity: ''
+      curCity: '',
+      films: []
+    }
+  },
+
+  methods: {
+    getCityName () {
+      /* eslint-disable */
+      let myCity = new BMap.LocalCity();
+      myCity.get((result) => {
+      this.curCity = result.name;
+      })
+    },
+
+    getFilms () {
+      axios.get('./static/api/homepage.json')
+        .then((response) => {
+          let result = response.data;
+          console.log(result);
+          if (result.status === 0) {
+            this.films = result.data.films;
+          } else {
+            console.log(result.msg);
+          }
+        })
+    },
+
+    actorsList (list) {
+      let arr = [];
+      arr = list.map(item => {
+        return item.name;
+      });
+
+      return arr.join(' ');
     }
   },
 
   created () {
     // 获取当前城市名称
-    /* eslint-disable */
-    let myCity = new BMap.LocalCity();
-    myCity.get((result) => {
-      this.curCity = result.name;
-    })
+    this.getCityName();
+    this.getFilms();
   },
 
   mounted () {
@@ -165,6 +237,67 @@ export default {
             border-radius: px2rem(20)
         }
     }
+  }
+}
+.film-list-content {
+  li {
+    display: flex;
+    margin: 0 px2rem(15);
+    padding: px2rem(15) 0;
+    border-bottom: px2rem(1) solid #ededed;
+  }
+
+  .img {
+    flex-shrink: 0;
+    width: px2rem(66);
+    height: px2rem(94);
+    img {
+      width: 100%;
+    }
+  }
+
+  .info {
+    min-width: 100px;
+    padding: 0 px2rem(10);
+    font-size: px2rem(14);
+    &>div {
+      width: 100%;
+      height: px2rem(22);
+      line-height: px2rem(22);
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .label {
+      color: #797d82;
+      margin-right: px2rem(2);
+    }
+    .name {
+      font-size: px2rem(16);
+      color: #191a1b;
+    }
+    .type {
+      font-size: px2rem(12);
+      color: #fff;
+      background: #d2d6dc;
+      padding: 0 px2rem(2);
+    }
+    .grade {
+      color: #ffb232;
+    }
+  }
+
+  .buy {
+    flex-shrink: 0;
+    align-self: center;
+    width: px2rem(50);
+    height: px2rem(26);
+    line-height: px2rem(26);
+    font-size: px2rem(14);
+    color: #ff5f16;
+    border: px2rem(1) solid #ff5f16;
+    text-align: center;
+    border-radius: px2rem(4);
   }
 }
 </style>
